a80dc9945a5e7a96933ea41b8ebcf0c20b0ae536
Archie Baldry <33762310+archiebaldry@users.noreply.github.com>
2024-11-30 19:33:52 +0000

    Initial commit

---
+++ src/client/java/archiebaldry/villagernames/VillagerNamesClient.java
@@ -0,0 +1,10 @@
+package archiebaldry.villagernames;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class VillagerNamesClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
